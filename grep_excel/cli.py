379b7a77c1360ae85ef42028be4285f95e32568b
stenzr <rkrohitkr01@gmail.com>
2024-08-13 21:43:03 +0530

    fix order of args

--- grep_excel/cli.py
+++ grep_excel/cli.py
@@ -4,8 +4,8 @@ from .grep import ExcelGrep
 
 def main():
     parser = argparse.ArgumentParser(description="Search Excel files for a pattern.")
-    parser.add_argument('directory', nargs='?', default=os.getcwd(), help="Directory to search (defaults to current directory)")
     parser.add_argument('pattern', help="Pattern to search for")
+    parser.add_argument('directory', nargs='?', default=os.getcwd(), help="Directory to search (defaults to current directory)")
     parser.add_argument('--exclude', '-v', help="Pattern to exclude")
     parser.add_argument('--print-row', action='store_true', help="Print entire row")
     
